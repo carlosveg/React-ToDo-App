7fc066d17cb47b8fd7aaaaf6606817b07041793a
Carlos Vega <carlosraymundovegagloria@gmail.com>
2021-04-21 21:00:59 -0500

    Comenzando el proyecto, se instalo react-calendar y se creo su componente.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import CalendarC from './components/Calendar/CalendarC';
+
+function App() {
+  return (
+    <CalendarC />
+  );
+}
+
+export default App;
---
+++ src/components/Calendar/CalendarC.jsx
@@ -0,0 +1,9 @@
+import Calendar from "react-calendar";
+
+function CalendarC() {
+  return (
+    <Calendar />
+  )
+}
+
+export default CalendarC;
